c1ab8e0b2818804f62ebe3ee024ccd5be27abc77
MD ASADULLAH <44978373+iamasad@users.noreply.github.com>
2025-02-06 05:24:32 +0530

    [FIX] naming convention fix

--- bsat_registration/__manifest__.py
+++ bsat_registration/__manifest__.py
@@ -1,10 +1,10 @@
 # bsat_registration/__manifest__.py
 {
     'name': 'BSAT Registration',
-    'version': '18.0.1',
+    'version': '18.0.0.1',
     'category': 'Custom',
     'author': 'Md Asadullah',
-    'depends': ['base'],
+    'depends': ['base','contacts'],
     'data': [
         'views/res_partner.xml',
     ],
